2c19cc340c5970c1ca343c70fd31262a39c0db2e
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-12-29 11:09:05 +0530

    Create route for adding a post

--- routes/api/postRoute.js
+++ routes/api/postRoute.js
@@ -1,8 +1,47 @@
 const express = require('express')
 const router = new express.Router()
+const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator')
+const Post = require('../../models/Post')
+const User = require('../../models/User')
 
 router.get('/api/posts', (req, res) => {
   res.send('Posts Route')
 })
+
+// @route      POST api/posts
+// @desc       Create a post
+// @access     Private
+router.post(
+  '/api/posts',
+  [
+    auth,
+    check('text', 'Text is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() })
+
+    try {
+      const user = await User.findById(req.user.id).select('-password')
+
+      const post = new Post({
+        text: req.body.text,
+        user: req.user.id,
+        name: user.name,
+        avatar: user.avatar
+      })
+
+      await post.save()
+      res.json(post)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 module.exports = router
