3414255eb657a33feec1f9cb9f9dd144092c689e
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-12-31 01:50:57 +0530

    Create Login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log('SUCCESS')
+  }
+
+  return (
+    <React.Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+            value={email}
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            required
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </React.Fragment>
+  )
 }
 
 export default Login
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -14,13 +14,13 @@ const Register = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault()
 
     if (password !== password2) {
       console.log("Passwords don't match")
     } else {
-      console.log(formData)
+      console.log('SUCCESS')
     }
   }
 
