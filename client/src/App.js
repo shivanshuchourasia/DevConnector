dd1d1706477b97887a23de0eee1384464ff5dc57
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-17 10:04:08 +0530

    Load User & Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/layout/Navbar'
@@ -6,27 +6,39 @@ import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
+import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
 
 // Redux
 import { Provider } from 'react-redux'
 import store from './store'
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <React.Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </React.Fragment>
-    </Router>
-  </Provider>
-)
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </React.Fragment>
+      </Router>
+    </Provider>
+  )
+}
 
 export default App
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types'
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED,AUTH_ERROR } from '../actions/types'
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -19,7 +19,15 @@ export default function(state = initialState, action) {
         isAuthenticated: true,
         loading: false
       }
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      }
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token')
       return {
         ...state,
