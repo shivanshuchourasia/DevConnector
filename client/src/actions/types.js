b32637e4e2c95026147d3cdc2e6d66d8da83beed
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-18 19:11:05 +0530

    Add spinner, handle profile state on dashboard

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -13,4 +13,5 @@ export const LOGIN_FAIL = 'LOGIN_FAIL'
 export const LOGOUT = 'LOGOUT'
 
 export const GET_PROFILE = 'GET_PROFILE'
+export const CLEAR_PROFILE = 'CLEAR_PROFILE'
 export const PROFILE_ERROR = 'PROFILE_ERROR'
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import Spinner from '../layout/Spinner'
 import PropTypes from 'prop-types'
 import { getCurrentProfile } from '../../actions/profile'
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
   useEffect(() => {
     getCurrentProfile()
   }, [])
 
-  return <div>Dashboard</div>
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user'> Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You haven't setup a profile, Please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  )
 }
 
 Dashboard.prototype = {
