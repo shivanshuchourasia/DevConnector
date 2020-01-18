e95bc6f9d22d1779878f64327bc0e0476923b318
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-18 11:05:42 +0530

    Create a protected route for dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+const Dashboard = () => {
+  return <div>Dashboard</div>
+}
+
+export default Dashboard
