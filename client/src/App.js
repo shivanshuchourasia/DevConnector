7801df5bc5354630d4b51883597b0160ebaedc5c
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-18 19:43:59 +0530

    Add CreateProfile Component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
+import CreateProfile from './components/profile-form/CreateProfile'
 import { loadUser } from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
 
@@ -36,6 +37,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </React.Fragment>
