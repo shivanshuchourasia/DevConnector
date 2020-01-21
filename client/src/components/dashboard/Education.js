95669bfaf859627e7710d4ef0089115d5b1c13ba
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-21 12:01:47 +0530

    Show education & experience on Dashboard

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Moment from 'react-moment'
+
+const Education = ({ education }) => {
+  const educations = education.map(edu => (
+    <tr key={edu._id}>
+      <td>{edu.school}</td>
+      <td className='hide-sm'>{edu.degree}</td>
+      <td>
+        <Moment format='DD/MM/YYYY'>{edu.from}</Moment> -{' '}
+        {edu.to ? ' Now' : <Moment format='DD/MM/YYYY'>{edu.to}</Moment>}
+      </td>
+      <td>
+        <button className='btn btn-danger'>Delete</button>
+      </td>
+    </tr>
+  ))
+
+  return (
+    <Fragment>
+      <h2 className='my-2'>Education Credentials</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>School/College</th>
+            <th className='hide-sm'>Degree</th>
+            <th className='hide-sm'>Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </table>
+    </Fragment>
+  )
+}
+
+Education.propTypes = {
+  // deleteExperience: PropTypes.func.isRequired,
+  education: PropTypes.array.isRequired
+}
+
+export default connect(null, {})(Education)
