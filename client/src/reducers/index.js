6c648e7281b21eb759a638b9522e6cc63a9f186c
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-25 23:59:46 +0530

    Minor Fix

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import alert from './alert'
 import auth from './auth'
 import profile from './profile'
+import post from './post'
 
 export default combineReducers({
   alert,
