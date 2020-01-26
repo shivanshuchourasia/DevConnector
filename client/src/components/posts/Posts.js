6820ad62a51635302e8397a0282c76f203284db7
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-26 23:52:02 +0530

    Add postForm component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { getPosts } from '../../actions/post'
 import Spinner from '../layout/Spinner'
 import PostItem from './PostItem'
+import PostForm from './PostForm'
 import PropTypes from 'prop-types'
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -20,21 +21,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         <i className='fas fa-user'></i> Welcome to the community!
       </p>
 
-      <div className='post-form'>
-        <div className='bg-primary p'>
-          <h3>Say Something...</h3>
-        </div>
-        <form className='form my-1'>
-          <textarea
-            name='text'
-            cols='30'
-            rows='5'
-            placeholder='Create a post'
-            required
-          ></textarea>
-          <input type='submit' className='btn btn-dark my-1' value='Submit' />
-        </form>
-      </div>
+      <PostForm />
 
       <div className='posts'>
         {posts.map(post => (
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from '../actions/types'
 
 const initialState = {
@@ -28,6 +29,12 @@ export default function(state = initialState, action) {
         posts: state.posts.map(post =>
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
+        loading: false
+      }
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
         loading: false
       }
     case DELETE_POST:
