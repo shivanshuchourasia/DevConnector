1a9da836c6ac96865fa5acd5f0b90effd093716e
Shivanshu Chourasia <shivansh4011@gmail.com>
2020-01-27 15:42:37 +0530

    Setup for deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('./db/db')
 
 const app = express()
@@ -11,6 +12,16 @@ app.use(require('./routes/api/userRoute'))
 app.use(require('./routes/api/authRoute'))
 app.use(require('./routes/api/profileRoute'))
 app.use(require('./routes/api/postRoute'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
